b77f08c8ee21d1bb4bb62efc84b6c73a70a6e25e
Rafael Teixeira <up202108831@edu.fe.up.pt>
2024-02-18 11:36:02 +0000

    Parallelogram is now visible on both sides

--- tp1/MyParallelogram.js
+++ tp1/MyParallelogram.js
@@ -21,7 +21,10 @@ export class MyParallelogram extends CGFobject {
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
-			2, 3, 0,
+			2, 3, 0
+			,
+			2, 1, 0,
+			0, 3, 2
 		];
 
 		//The defined indices (and corresponding vertices)
